f319893d959f9d8a8ec9dacfcf0787a4fb80ec83
yuraostapenko <41306905+yuraostapenko@users.noreply.github.com>
2018-10-25 12:23:02 +0300

    added devtools

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -6,8 +6,6 @@ import { connectRouter } from 'connected-react-router'
 
 export const history = createBrowserHistory()
 
-const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-const  store = createStore(connectRouter(history)(rootReducer), compose(applyMiddleware(thunk), DevTools));
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore (connectRouter(history)(rootReducer), composeEnhancers(applyMiddleware(thunk)));
 export default store;
